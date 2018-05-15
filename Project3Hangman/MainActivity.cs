94c8c59d0c22ae8c9cb3dfd316b656e184881e51
Jess Costello <jessica.a.costello@gmail.com>
2018-05-15 14:10:05 +1200

    onAnyLetterClick set up

--- Project3Hangman/MainActivity.cs
+++ Project3Hangman/MainActivity.cs
@@ -2,18 +2,113 @@
 using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
+using System;
 
 namespace Project3Hangman
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        Button btnA;
+        Button btnB;
+        Button btnC;
+        Button btnD;
+        Button btnE;
+        Button btnF;
+        Button btnG;
+        Button btnH;
+        Button btnI;
+        Button btnJ;
+        Button btnK;
+        Button btnL;
+        Button btnM;
+        Button btnN;
+        Button btnO;
+        Button btnP;
+        Button btnQ;
+        Button btnR;
+        Button btnS;
+        Button btnT;
+        Button btnU;
+        Button btnV;
+        Button btnW;
+        Button btnX;
+        Button btnY;
+        Button btnZ;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Game);
+
+            btnA = FindViewById<Button>(Resource.Id.btnA);
+            btnB = FindViewById<Button>(Resource.Id.btnB);
+            btnC = FindViewById<Button>(Resource.Id.btnC);
+            btnD = FindViewById<Button>(Resource.Id.btnD);
+            btnE = FindViewById<Button>(Resource.Id.btnE);
+            btnF = FindViewById<Button>(Resource.Id.btnF);
+            btnG = FindViewById<Button>(Resource.Id.btnG);
+            btnH = FindViewById<Button>(Resource.Id.btnH);
+            btnI = FindViewById<Button>(Resource.Id.btnI);
+            btnJ = FindViewById<Button>(Resource.Id.btnJ);
+            btnK = FindViewById<Button>(Resource.Id.btnK);
+            btnL = FindViewById<Button>(Resource.Id.btnL);
+            btnM = FindViewById<Button>(Resource.Id.btnM);
+            btnN = FindViewById<Button>(Resource.Id.btnN);
+            btnO = FindViewById<Button>(Resource.Id.btnO);
+            btnP = FindViewById<Button>(Resource.Id.btnP);
+            btnQ = FindViewById<Button>(Resource.Id.btnQ);
+            btnR = FindViewById<Button>(Resource.Id.btnR);
+            btnS = FindViewById<Button>(Resource.Id.btnS);
+            btnT = FindViewById<Button>(Resource.Id.btnT);
+            btnU = FindViewById<Button>(Resource.Id.btnU);
+            btnV = FindViewById<Button>(Resource.Id.btnV);
+            btnW = FindViewById<Button>(Resource.Id.btnW);
+            btnX = FindViewById<Button>(Resource.Id.btnX);
+            btnY = FindViewById<Button>(Resource.Id.btnY);
+            btnZ = FindViewById<Button>(Resource.Id.btnZ);
+
+            btnA.Click += onAnyLetterClick;
+            btnB.Click += onAnyLetterClick;
+            btnC.Click += onAnyLetterClick;
+            btnD.Click += onAnyLetterClick;
+            btnE.Click += onAnyLetterClick;
+            btnF.Click += onAnyLetterClick;
+            btnG.Click += onAnyLetterClick;
+            btnH.Click += onAnyLetterClick;
+            btnI.Click += onAnyLetterClick;
+            btnJ.Click += onAnyLetterClick;
+            btnK.Click += onAnyLetterClick;
+            btnL.Click += onAnyLetterClick;
+            btnM.Click += onAnyLetterClick;
+            btnN.Click += onAnyLetterClick;
+            btnO.Click += onAnyLetterClick;
+            btnP.Click += onAnyLetterClick;
+            btnQ.Click += onAnyLetterClick;
+            btnR.Click += onAnyLetterClick;
+            btnS.Click += onAnyLetterClick;
+            btnT.Click += onAnyLetterClick;
+            btnU.Click += onAnyLetterClick;
+            btnV.Click += onAnyLetterClick;
+            btnW.Click += onAnyLetterClick;
+            btnX.Click += onAnyLetterClick;
+            btnY.Click += onAnyLetterClick;
+            btnZ.Click += onAnyLetterClick;
+        }
+
+        private void onAnyLetterClick(object sender, EventArgs e)
+        {
+            string letter = (sender as Button).Text;
+            Toast.MakeText(this, letter, ToastLength.Long).Show();
+            // disable button so it can't be clicked again
+            (sender as Button).Enabled = false;
+            // check if the letter is in the word
+            // if letter is in the word display it
+            // if it isn't change picture
+            // do some kind of scoring system
+            
         }
     }
 }
