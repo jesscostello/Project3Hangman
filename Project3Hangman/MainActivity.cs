5c7c7b56e66c0b20884074d2f0e25c126a804e37
Jess Costello <jessica.a.costello@gmail.com>
2018-06-19 14:21:10 +1200

    main screen

--- Project3Hangman/MainActivity.cs
+++ Project3Hangman/MainActivity.cs
@@ -15,12 +15,32 @@ namespace Project3Hangman
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        Button btnPlay;
+        EditText etName;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
             SetContentView(Resource.Layout.Main);
+
+            etName = FindViewById<EditText>(Resource.Id.etName);
+            btnPlay = FindViewById<Button>(Resource.Id.btnPlay);
+
+            btnPlay.Click += BtnPlay_Click;
+            etName.TextChanged += EtName_TextChanged;
+        }
+
+        private void EtName_TextChanged(object sender, Android.Text.TextChangedEventArgs e)
+        {
+            btnPlay.Enabled = true;
+        }
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            Player.name = etName.Text;
+            StartActivity(typeof(HangmanActivity));
         }
     }
 }
